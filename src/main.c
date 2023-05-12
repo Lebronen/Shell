d6a61a571eb8317fa7a03a17758e56debe98f7d8
Lebronen <96152449+Lebronen@users.noreply.github.com>
2023-05-12 18:01:43 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,38 +3,42 @@
 #include <stdio.h>
 
 int main(void){
+	
 	noeud *courant;
 	noeud *racine;
-	FILE* fp = fopen("../test.txt", "r");
 	racine = new_node(NULL);
 	racine->est_dossier = true;
 	racine->nom[0] = 0;
 	racine->racine = racine;
 	courant = racine;
+	FILE* fp = fopen("test.txt", "r");
 	if(fp != NULL){
 		char buffer[1024];
 		while(!feof(fp)){
 			if(fgets(buffer, sizeof(buffer) - 1, fp) != NULL){
-				printf("%s", buffer);
+				courant = commande(buffer, racine, courant);
 			}
 		}
-		puts("");
+
 		fclose(fp);
 	}
-	mkdir("ronen", courant);
 	
-	ls("", courant);
+	/*
+	mkdir("ronen", courant);
+	//print(courant);
+	//ls("ronen", courant);
 	//courant = cd("ronen", courant);
 	touch("ronen/yohan", courant);
 	touch("ronen/duo_de_fou", courant);
-	touch("ronen/jojo", courant);
-	print(courant);
+	mkdir("ronen/jojo", courant);
+	touch("ronen/jojo/jotaro", courant);
 	//ls(courant);
-	//courant = cd("..", courant);
-	rm(courant, "ronen/yohan");
-	courant = cd("ronen", courant);
+	courant = cd("ronen/jojo", courant);
 	ls("", courant);
-
+	//printf("%d\n", subslash("ronen/"));
+	courant = cd("..", courant);
+	ls("", courant);
+*/
 	rm_free(racine->fils);
 	free(racine);
 	
@@ -62,4 +66,27 @@ char *up_str(char *s)
 		t[i] = s[i] - 32;
 	}
 	return t;
+}
+
+noeud *commande(char *s, noeud *racine, noeud *courant)
+{
+	printf("%s", s);
+	if (!strncmp(s, "mkdir", 5))
+		mkdir(s + 6, courant);
+	else if (!strncmp(s, "ls", 2))
+		ls(s + 3, courant);
+	else if (!strncmp(s, "touch", 5))
+		touch(s + 6, courant);
+	else if (!strncmp(s, "rm", 2))
+		rm(courant, s + 3);
+	else if (!strncmp(s, "print", 5))
+		print(courant);
+	else if (!strncmp(s, "cd", 2))
+		courant = cd(s + 3, courant);
+	else if (!strncmp(s, "pwd", 3))
+		pwd(courant);
+	else
+		printf("commande non reconnue\n");
+
+	return courant;
 }
